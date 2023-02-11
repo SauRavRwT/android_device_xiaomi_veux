2723d790d1653b81c1618795bb59015daf5f99bc
SauRavRwT <souravrawat142@gmail.com>
2023-02-11 09:38:29 +0530

    veux: It's Xtended time

---
+++ xtended_veux.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from veux device
+$(call inherit-product, device/xiaomi/veux/device.mk)
+
+# Inherit some common Xtended Stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := xtended_veux
+PRODUCT_DEVICE := veux
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO X4 Pro 5G
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Xtended Stuff with GApps
+XTENDED_BUILD_TYPE := UNOFFICIAL
+XTENDED_BUILD_MAINTAINER := SauRavRwT
+PROCESSOR_MODEL := SM6375
+WITH_GAPPS := true
+XTENDED_BUILD_DONATE_URL := https://www.paypal.me/RiteshKrSharma
+XTENDED_BUILD_SUPPORT_URL := https://t.me/veux_builds
+
+# Inherit some common device props
+TARGET_USES_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+BUILD_FINGERPRINT := POCO/veux_p_global/veux:11/RKQ1.211001.001/V13.0.10.0.RKCMIXM:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="veux-user 11 RKQ1.211001.001 V13.0.10.0.RKCMIXM release-keys"
